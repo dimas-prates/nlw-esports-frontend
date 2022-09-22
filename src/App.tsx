73b49587d0d8ca85855eab51048e4c41825136dc
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-09-21 21:03:00 -0300

    :sparkles: feat(app.tsx): set mini games profiles, button for posting event, etc. using tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,80 @@
-import './App.css'
+import './styles/main.css'
+import logoImage from './assets/nlw-esports-logo.svg'
+import { MagnifyingGlassPlus } from 'phosphor-react'
 
 function App() {
   return (
-    <>
-    </>
+    <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
+      <img src={logoImage} alt="eSports logo" />
+
+      <h1 className='text-6xl text-white font-black mt-20 '>Your <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> is here</h1>
+
+      <div className='grid grid-cols-6 gap-6 mt-16'>
+
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/image1.png" alt="game image" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Game Name</strong>
+            <span className='text-zinc-300 text-sm block'>4 notifications</span>
+          </div>
+        </a>
+
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/image2.png" alt="game image" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Game Name</strong>
+            <span className='text-zinc-300 text-sm block'>4 notifications</span>
+          </div>
+        </a>
+
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/image3.png" alt="game image" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Game Name</strong>
+            <span className='text-zinc-300 text-sm block'>4 notifications</span>
+          </div>
+        </a>
+
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/image4.png" alt="game image" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Game Name</strong>
+            <span className='text-zinc-300 text-sm block'>4 notifications</span>
+          </div>
+        </a>
+
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/image5.png" alt="game image" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Game Name</strong>
+            <span className='text-zinc-300 text-sm block'>4 notifications</span>
+          </div>
+        </a>
+
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/image6.png" alt="game image" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Game Name</strong>
+            <span className='text-zinc-300 text-sm block'>4 notifications</span>
+          </div>
+        </a>
+      </div>
+
+      <div className='pt-1 bg-nlw-gradient mt-8 self-stretch rounded-lg overflow-hidden'>
+        <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
+          <div>
+            <strong className='text-2xl text-white font-black block'>Couldn't find your duo?</strong>
+            <span className='text-zinc-400'>Publish an event to find new players</span>
+          </div>
+          <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3'>
+            <MagnifyingGlassPlus size={24} />
+            Post event
+          </button>
+        </div>
+      </div>
+
+    </div>
+
   )
 }
 
